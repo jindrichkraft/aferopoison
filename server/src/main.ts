37fe0048a120383b019131dec8d99f3be005cf5d
Jindřich Kraft <jindrich.kraft@proton.me>
2023-03-29 20:14:49 +0200

    BE: Adding and setting up pg

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Pool } from 'pg';
 
-import { DEFAULT_EXPRESS_PORT } from './utils/constants';
+import {
+  DEFAULT_EXPRESS_PORT,
+  DEFAULT_POSTGRES_PORT,
+  DEFAULT_POSTGRES_HOST,
+} from './utils/constants';
 
 // Setup
 const app = express();
@@ -12,8 +17,19 @@ dotenv.config();
 
 // Ports
 const expressPort = Number(process.env.EXPRESS_PORT) || DEFAULT_EXPRESS_PORT;
+const postgresPort = Number(process.env.POSTGRES_PORT) || DEFAULT_POSTGRES_PORT;
+
+// PostgreSQL Pool
+export const pool = new Pool({
+  host: process.env.DB_HOST || DEFAULT_POSTGRES_HOST,
+  user: process.env.DB_USER || '',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || '',
+  port: postgresPort,
+});
 
 // Express Server
 app.listen(expressPort, () => {
   console.log(`[STATUS] Express listening on port ${expressPort}...`);
+  console.log(`[STATUS] PostgreSQL running on port ${postgresPort}...`);
 });

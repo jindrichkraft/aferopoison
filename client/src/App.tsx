fa48d23505fc7b0043278389809071150c7cbd06
Jindřich Kraft <jindrich.kraft@proton.me>
2023-03-30 19:21:11 +0200

    FE: Protected routes system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,41 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import { useAuth } from './hooks/auth';
 
-const App = (): JSX.Element => (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/login" element={<LoginPage />} />
-  </Routes>
-);
+interface IProtectedRouteProps {
+  condition: boolean;
+  fallbackPath: string;
+}
+
+const ProtectedRoute = ({
+  condition,
+  fallbackPath,
+}: IProtectedRouteProps): JSX.Element => {
+  return condition ? <Outlet /> : <Navigate to={fallbackPath} replace />;
+};
+
+const App = (): JSX.Element => {
+  const { auth } = useAuth();
+
+  if (typeof auth === 'undefined') {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route element={<ProtectedRoute condition={!auth} fallbackPath="/" />}>
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+      <Route
+        element={<ProtectedRoute condition={!!auth} fallbackPath="/login" />}
+      >
+        <Route path="/logout" element={<HomePage />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;

0c8ad46a55df16c8b52eb520268a8feb8937c559
Jindřich Kraft <jindrich.kraft@proton.me>
2023-03-30 12:45:41 +0200

    BE: Creating the /auth/login route and generating JWT tokens

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -6,9 +6,12 @@ import {
   GET_USER_BY_USERNAME_QUERY,
   CREATE_NEW_USER_QUERY,
 } from '../queries/auth';
+import { generateAccessTokenData } from '../utils/jwt';
 import {
   PASSWORD_SALT_ROUNDS,
+  CREATED_CODE,
   BAD_REQUEST_CODE,
+  UNAUTHORIZED_CODE,
   SERVER_ERROR_CODE,
   CONFLICT_CODE,
 } from '../utils/constants';
@@ -49,11 +52,71 @@ authRouter.post('/register', async (req, res) => {
         hashedPassword,
       ])
     ).rows[0];
-    return res.json({
+
+    // Sending the access data
+    return res.status(CREATED_CODE).json({
       success: true,
       message: 'Successfully created a new user!',
       data: {
-        id: newUser.user_id,
+        userInfo: {
+          displayName: newUser.display_name,
+        },
+        tokenData: generateAccessTokenData(newUser.user_id),
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(SERVER_ERROR_CODE).json({
+      success: false,
+      message: 'Server error!',
+      data: null,
+    });
+  }
+});
+
+authRouter.post('/login', async (req, res) => {
+  try {
+    // Validating input data
+    const { username, password } = req.body;
+    if (!username || !password)
+      return res.status(BAD_REQUEST_CODE).json({
+        success: false,
+        message: 'Invalid input data!',
+        data: null,
+      });
+
+    // Checking if user exists
+    const existingUser = await pool.query(GET_USER_BY_USERNAME_QUERY, [
+      username,
+    ]);
+    if (existingUser.rowCount === 0)
+      return res.status(UNAUTHORIZED_CODE).json({
+        success: false,
+        message: 'User with this username does not exist!',
+        data: null,
+      });
+
+    // Verifying the password
+    const validPassword = await bcrypt.compare(
+      password,
+      existingUser.rows[0].password
+    );
+    if (!validPassword)
+      return res.status(UNAUTHORIZED_CODE).json({
+        success: false,
+        message: 'Invalid password!',
+        data: null,
+      });
+
+    // Sending the access data
+    return res.json({
+      success: true,
+      message: 'Successfully logged in!',
+      data: {
+        userInfo: {
+          displayName: existingUser.rows[0].display_name,
+        },
+        tokenData: generateAccessTokenData(existingUser.rows[0].user_id),
       },
     });
   } catch (error) {
--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -8,9 +8,12 @@ export const AUTH_ROUTE_PATH = '/auth';
 
 // Auth
 export const PASSWORD_SALT_ROUNDS = 10;
+export const AUTH_TOKEN_TYPE = 'Bearer';
+export const AUTH_TOKEN_EXPIRATION_IN_HOURS = 18;
 
 // Status Codes
 export const CREATED_CODE = 201;
 export const BAD_REQUEST_CODE = 400;
+export const UNAUTHORIZED_CODE = 401;
 export const CONFLICT_CODE = 409;
 export const SERVER_ERROR_CODE = 500;

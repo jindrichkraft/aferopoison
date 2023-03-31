ff6a105308b9e9365321609e24fd3be5699f06fd
Jindřich Kraft <jindrich.kraft@proton.me>
2023-03-31 20:11:40 +0200

    BE: Adding a GET method for the project route

---
+++ server/src/queries/project.ts
@@ -0,0 +1,1 @@
+export const GET_ALL_PROJECTS_QUERY = 'SELECT * FROM Projects';
--- server/src/routes/project.ts
+++ server/src/routes/project.ts
@@ -1,5 +1,24 @@
 import { Router } from 'express';
 
+import { pool } from '../main';
+import { GET_ALL_PROJECTS_QUERY } from '../queries/project';
+import { authorize } from '../middlewares/auth';
+import { SERVER_ERROR_CODE } from '../utils/constants';
+
 const projectRouter = Router();
+
+projectRouter.get('/', authorize, async (_, res) => {
+  try {
+    const projects = await pool.query(GET_ALL_PROJECTS_QUERY);
+    return res.json(projects.rows || []);
+  } catch (error) {
+    console.log(error);
+    return res.status(SERVER_ERROR_CODE).json({
+      success: false,
+      message: 'Server error!',
+      data: null,
+    });
+  }
+});
 
 export default projectRouter;

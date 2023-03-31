221aaa6c0da4ca6813a0534fa7e2b95bd47a68a5
Jindřich Kraft <jindrich.kraft@proton.me>
2023-03-31 20:04:06 +0200

    SQL: Adding a declaration for the Projects table

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,8 +1,15 @@
 DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Projects;
 
 CREATE TABLE Users (
   user_id SERIAL PRIMARY KEY,
-  username VARCHAR(32),
-  display_name VARCHAR(48),
-  password VARCHAR
+  username VARCHAR(32) NOT NULL,
+  display_name VARCHAR(48) NOT NULL,
+  password VARCHAR NOT NULL
+);
+
+CREATE TABLE Projects (
+  project_id SERIAL PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  description TEXT
 );

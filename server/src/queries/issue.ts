5f423d682c4508bf1d756751f95300e68574cf81
Jindřich Kraft <jindrich.kraft@proton.me>
2023-04-01 18:36:42 +0200

    BE: Adding a GET route for feching a single issue

--- server/src/queries/issue.ts
+++ server/src/queries/issue.ts
@@ -3,6 +3,13 @@ export const GET_ALL_ISSUES_QUERY =
   'FROM Issues ' +
   'JOIN UserProjects ON Issues.project_id = UserProjects.project_id ' +
   'WHERE UserProjects.user_id = $1';
+export const GET_ISSUE_BY_ID_QUERY =
+  'SELECT Issues.*, u1.display_name AS added_by_name, u2.display_name AS assigned_to_name  ' +
+  'FROM Issues ' +
+  'JOIN Users u1 ON Issues.added_by = u1.user_id ' +
+  'JOIN Users u2 ON Issues.assigned_to = u2.user_id ' +
+  'JOIN UserProjects ON Issues.project_id = UserProjects.project_id ' +
+  'WHERE UserProjects.user_id = $1 AND Issues.issue_id = $2';
 export const GET_ALL_ISSUES_ASSIGNED_TO_USER_QUERY =
   'SELECT Issues.*, Projects.name AS project_name ' +
   'FROM Issues ' +
--- server/src/routes/issue.ts
+++ server/src/routes/issue.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import { pool } from '../main';
-import { GET_ALL_ISSUES_ASSIGNED_TO_USER_QUERY } from '../queries/issue';
+import {
+  GET_ISSUE_BY_ID_QUERY,
+  GET_ALL_ISSUES_ASSIGNED_TO_USER_QUERY,
+} from '../queries/issue';
 import { authorize } from '../middlewares/auth';
-import { SERVER_ERROR_CODE } from '../utils/constants';
+import { BAD_REQUEST_CODE, SERVER_ERROR_CODE } from '../utils/constants';
 
 const issueRouter = Router();
 
@@ -17,6 +20,34 @@ issueRouter.get('/', authorize, async (req, res) => {
       success: true,
       message: null,
       data: issues.rows || [],
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(SERVER_ERROR_CODE).json({
+      success: false,
+      message: 'Server error!',
+      data: null,
+    });
+  }
+});
+
+issueRouter.get('/:id', authorize, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const issue = await pool.query(GET_ISSUE_BY_ID_QUERY, [req.user.id, id]);
+
+    // Validating if issue with the given ID exists
+    if (issue.rowCount === 0)
+      return res.status(BAD_REQUEST_CODE).json({
+        success: false,
+        message: 'Project with this ID does not exist!',
+        data: null,
+      });
+
+    return res.json({
+      success: true,
+      message: null,
+      data: issue.rows[0] || null,
     });
   } catch (error) {
     console.log(error);
